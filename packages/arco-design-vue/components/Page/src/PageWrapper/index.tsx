ed3a14a022a1fd630a9f9689d196f0eaaf1ccefd
yiwwhl <yiwwhl@qq.com>
2023-09-28 00:31:52 +0800

    fix(arco-vue): fix PageWrapper ts error

--- packages/arco-design-vue/components/Page/src/PageWrapper/index.tsx
+++ packages/arco-design-vue/components/Page/src/PageWrapper/index.tsx
@@ -3,6 +3,7 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   setup(props, { slots }) {
+    console.log("props", props);
     return () => {
       return (
         <BasicWrapper heightMode="fixed">{slots.default?.()}</BasicWrapper>
