f2696d09e13d04027d3f1191b2c0e0d11c299323
yiwwhl <yiwwhl@qq.com>
2023-11-16 18:16:52 +0800

    docs(arco-vue): remove console

--- packages/arco-design-vue/components/ProForm/src/hooks/useForm.ts
+++ packages/arco-design-vue/components/ProForm/src/hooks/useForm.ts
@@ -27,7 +27,6 @@ function useForm(props: UseFormProps): UseForm {
     let newSchema = reactive({
       raw: {},
     }) as any;
-    console.log("process");
     const keys = Object.keys(schema);
     for (let i = 0; i < keys.length; i++) {
       handleAsyncOrSync(
