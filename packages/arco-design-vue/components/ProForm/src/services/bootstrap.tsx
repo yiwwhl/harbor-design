bcff5148d5f8c8517a3c00ebb0c81e185ff96a65
yiwwhl <yiwwhl@qq.com>
2023-11-17 17:03:25 +0800

    feat: handleAddClick and handleDelete click

--- packages/arco-design-vue/components/ProForm/src/services/bootstrap.tsx
+++ packages/arco-design-vue/components/ProForm/src/services/bootstrap.tsx
@@ -8,26 +8,41 @@ export function bootstrap(register: Register) {
   const GroupWrapper =
     globalConfig.Components.ProForm.layout?.Group ?? Fragment;
   const ListWrapper = globalConfig.Components.ProForm.layout?.List ?? Fragment;
-  const AddButton =
-    globalConfig.Components.ProForm.opt?.AddButton ??
-    (({ schema, model }: any) => {
-      function handleAddClick() {
-        model[schema.field].push({
-          ...register.immutableModel[schema.field][0],
-        });
-      }
-      return <Button onClick={handleAddClick}>添加</Button>;
+
+  function handleAddClick(schema: any, model: any) {
+    model[schema.field].push({
+      ...register.immutableModel[schema.field][0],
     });
-  const DeleteButton =
-    globalConfig.Components.ProForm.opt?.DeleteButton ??
-    (({ schema, model, index }: any) => {
-      function handleDeleteClick() {
-        model[schema.field].splice(index, 1);
-        // TODO: maybe improve
-        register.registerInstance.formRef.clearValidate();
-      }
-      return <Button onClick={handleDeleteClick}>删除</Button>;
-    });
+  }
+
+  function handleDeleteClick(schema: any, model: any, index: any) {
+    model[schema.field].splice(index, 1);
+    // TODO: maybe improve
+    register.registerInstance.formRef.clearValidate();
+  }
+
+  const AddButton = globalConfig.Components.ProForm.opt?.AddButton
+    ? ({ schema, model }: any) =>
+        globalConfig.Components.ProForm.opt?.AddButton(() =>
+          handleAddClick(schema, model)
+        )
+    : ({ schema, model }: any) => {
+        return (
+          <Button onClick={() => handleAddClick(schema, model)}>添加</Button>
+        );
+      };
+  const DeleteButton = globalConfig.Components.ProForm.opt?.DeleteButton
+    ? ({ schema, model, index }: any) =>
+        globalConfig.Components.ProForm.opt?.DeleteButton(() =>
+          handleDeleteClick(schema, model, index)
+        )
+    : ({ schema, model, index }: any) => {
+        return (
+          <Button onClick={() => handleDeleteClick(schema, model, index)}>
+            删除
+          </Button>
+        );
+      };
 
   return {
     GroupWrapper,
