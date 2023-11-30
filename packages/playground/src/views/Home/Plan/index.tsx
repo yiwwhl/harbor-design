938637f096fd37940551589ebf867dd3fe0a0392
yiwwhl <yiwwhl@qq.com>
2023-11-30 23:48:22 +0800

    docs: add recent plans

--- packages/playground/src/views/Home/Plan/index.tsx
+++ packages/playground/src/views/Home/Plan/index.tsx
@@ -8,6 +8,10 @@ export default defineComponent({
         <PageWrapper title="计划">
           <div>持续优化动态表单</div>
           <div>动态表单仍有大量功能需要实现，包括更深层的抽象</div>
+          <div>
+            近期计划：继续闭环动态表单的定制化能力，包括单个 schema
+            的定制和整体的定制
+          </div>
         </PageWrapper>
       );
     };
