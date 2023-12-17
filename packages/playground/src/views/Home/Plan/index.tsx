1fd52ee09543c487a3f3fef0e86d187e51e0dea7
yiwwhl <yiwwhl@qq.com>
2023-12-17 13:20:43 +0800

    feat: remove plan

--- packages/playground/src/views/Home/Plan/index.tsx
+++ packages/playground/src/views/Home/Plan/index.tsx
@@ -7,7 +7,6 @@ export default defineComponent({
 			return (
 				<PageWrapper title="计划">
 					<div>- 持续优化动态表单, 文档完善</div>
-					<div>- 重构 BasicWrapper 及 PageWrapper</div>
 					<div>- 项目增加 nest 或 koa server 用于学习</div>
 				</PageWrapper>
 			);
