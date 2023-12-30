f23e9009c9ae20bb5b8d4dd736addc69157d7a7e
yiwwhl <yiwwhl@qq.com>
2023-12-30 22:40:51 +0800

    fix: fix bug of UserCenter

--- packages/playground/src/layout/components/Header/index.tsx
+++ packages/playground/src/layout/components/Header/index.tsx
@@ -3,30 +3,31 @@ import styles from "./index.module.scss";
 import Logo from "@/assets/image/harbor.svg?component";
 import { ImageCollector } from "@/plugins/ImageCollector/src";
 import { useRouter } from "vue-router";
+import useBasicSettingStore from "@/store/modules/basicSetting";
 
 export default defineComponent({
 	setup() {
 		const router = useRouter();
+		const basicSettingStore = useBasicSettingStore();
 
 		return () => {
 			return (
 				<div class={styles.header}>
-					<div
-						class={styles.title}
-						onClick={() => {
-							router.push("/");
-						}}
-					>
+					<div class={styles.title}>
 						<Logo class={styles.logo} />
 						Harbor Design
 					</div>
 					<div
 						class={styles.meta}
-						onClick={() =>
+						onClick={() => {
+							basicSettingStore.updateSidebarMenuKeys(
+								"selectedKeys",
+								"UserCenter",
+							);
 							router.push({
 								name: "UserCenter",
-							})
-						}
+							});
+						}}
 					>
 						<img class={styles.avatar} src={ImageCollector.getImage("user")} />
 						<div class={styles.uinfo}>
