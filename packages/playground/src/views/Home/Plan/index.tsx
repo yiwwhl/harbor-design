0e444121ad7e1b8547639707e46305a66da7f364
yiwwhl <yiwwhl@qq.com>
2024-01-28 18:49:56 +0800

    docs: update project plan

--- packages/playground/src/views/Home/Plan/index.tsx
+++ packages/playground/src/views/Home/Plan/index.tsx
@@ -4,7 +4,11 @@ import { defineComponent } from "vue";
 export default defineComponent({
 	setup() {
 		return () => {
-			return <PageWrapper title="计划">近期：接入用户登录系统</PageWrapper>;
+			return (
+				<PageWrapper title="计划">
+					规范接口设计，考虑从 Prisma 迁移到 drizzle
+				</PageWrapper>
+			);
 		};
 	},
 });
