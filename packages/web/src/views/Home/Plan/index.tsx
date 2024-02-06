f6115124505f952e0a115c78aba4ab5c693153b5
yiwwhl <yiwwhl@qq.com>
2024-02-06 18:26:44 +0800

    chore: update project plan

--- packages/web/src/views/Home/Plan/index.tsx
+++ packages/web/src/views/Home/Plan/index.tsx
@@ -4,7 +4,12 @@ import { defineComponent } from "vue";
 export default defineComponent({
 	setup() {
 		return () => {
-			return <PageWrapper title="计划">开发 ProTable 及用户列表</PageWrapper>;
+			return (
+				<PageWrapper title="计划">
+					<div>开发 ProTable 及用户列表</div>
+					<div>沉淀新增组件相关文档</div>
+				</PageWrapper>
+			);
 		};
 	},
 });
