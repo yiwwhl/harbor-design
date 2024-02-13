eebfa88b0d207914e5400e2aca02856de7adf434
yiwwhl <yiwwhl@qq.com>
2024-02-13 11:00:57 +0800

    fix: fix warning of props and wrong style of avatar display

--- packages/web/src/bootstrap/ProFormRuntimeDoms/Item/index.tsx
+++ packages/web/src/bootstrap/ProFormRuntimeDoms/Item/index.tsx
@@ -2,6 +2,11 @@ import { defineComponent } from "vue";
 import styles from "./index.module.scss";
 
 export default defineComponent({
+	props: {
+		show: {
+			type: Boolean,
+		},
+	},
 	setup(_, { slots }) {
 		return () => {
 			return (
