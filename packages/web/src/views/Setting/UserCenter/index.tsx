5005c62df16ebb4238840e6795b5106f8e855b12
yiwwhl <yiwwhl@qq.com>
2024-02-14 10:42:31 +0800

    feat: update userCenter

--- packages/web/src/views/Setting/UserCenter/index.tsx
+++ packages/web/src/views/Setting/UserCenter/index.tsx
@@ -4,6 +4,7 @@ import AvatarUploader from "@/components/advanced/Uploader/AvatarUploader";
 import useUserStore from "@/store/modules/user";
 import { Button, Space } from "@arco-design/web-vue";
 import { defineComponent } from "vue";
+import styles from "./index.module.scss";
 
 export default defineComponent({
 	setup() {
@@ -19,14 +20,20 @@ export default defineComponent({
 
 		return () => {
 			return (
-				<PageWrapper title="个人中心">
-					<Space direction="vertical" size={12}>
-						{!isDemo && <AvatarUploader round />}
-						<Button type="primary" onClick={handleLogout}>
-							退出登录
-						</Button>
-					</Space>
-				</PageWrapper>
+				<div class={styles.userCenterWrapper}>
+					<PageWrapper class={styles.userInfo}>
+						<Space direction="vertical" align="center" size={20}>
+							{!isDemo && <AvatarUploader round />}
+							<div>{userStore.user.nickname}</div>
+							<Button type="primary" onClick={handleLogout}>
+								退出登录
+							</Button>
+						</Space>
+					</PageWrapper>
+					<PageWrapper class={styles.userSummary}>
+						<div>Summary dev inprogress.</div>
+					</PageWrapper>
+				</div>
 			);
 		};
 	},
